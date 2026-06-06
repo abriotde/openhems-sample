65590afc05c7a4ebf5e4a638532d7705d6f76a1b
Albéric <contact@openhomesystem.com>
2026-06-06 06:49:39 +0200

    FIX for addon (#33)
    * FIX action/docker_push_test
    
    * FIX action/docker_push_test
    
    * Update docker_push_test.yml
    
    * MOD actions : to the last tags of sub-actions.
    
    * FIX actions
    
    * Try FIX openhems-addon by specifying the  typing-extensions version?
    
    * FIX action update sub-actions
    
    * Update docker_push.yml
    
    * FIX run from other path (Docker, Home-Assistant add-on... )
    FIX : start allways the Web server.
    
    * Update docker_push.yml
    
    * Update Version
    
    * FIX webpages and update
    
    ---------
    
    Co-authored-by: alberic <alberic.briot@osiris-team.fr>

--- src/openhems/modules/web/pages/04_About.py
+++ src/openhems/modules/web/pages/04_About.py
@@ -18,7 +18,12 @@ def about_page():
     st.markdown(f"""
     **{conf.getName()}** version {conf.getVersion()}
     Licence : {conf.getLicence()}
-    [Site web]({conf.getUrls()['Homepage']}) | [Documentation]({conf.getUrls()['Documentation']})
+    [Site web]({conf.getUrls().get('Homepage', '#')}) | [Documentation]({conf.getUrls().get('Documentation', '#')})
+    [Source code]({conf.getUrls().get('Repository', '#')}) | [Bug tracker]({conf.getUrls().get('Issues', '#')})
+    Contact : {conf.getContact()}
+
+    N'hésitez pas à contribuer au projet sur [GitHub]({conf.getUrls().get('Source', '#')})
     """)
+    # Merci à tous les contributeurs qui ont participé au développement d'OpenHEMS :
 
 about_page()
